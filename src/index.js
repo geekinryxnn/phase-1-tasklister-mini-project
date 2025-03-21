e09a65c63d43215c0209016179c6877f63eb4d56
geekinryxnn <ryankiratu67@gmail.com>
2025-03-21 08:54:44 +0300

    new changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const taskForm = document.getElementById("create-task-form");
+const taskList = document.getElementById("tasks");
+
+taskForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const newTask = document.getElementById("new-task-description").value;
+  const taskItem = document.createElement("li");
+  taskItem.textContent = newTask;
+  taskList.appendChild(taskItem);
+  taskForm.reset();
+});
